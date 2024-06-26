9ab887c423c4bb4b13589b960294dea0f670210d
ritesh <rieshdhapatepatil@gmail.com>
2024-06-26 20:57:26 +0530

    changes

--- index.js
+++ index.js
@@ -10,41 +10,33 @@ const BASE_PATH =
 const proxy = httpProxy.createProxy();
 
 app.use("/:subdomain", (req, res) => {
-  try {
-    console.log("data", req.originalUrl);
-    const subdomain = req.originalUrl;
+  console.log("data", req.originalUrl);
+  const subdomain = req.originalUrl;
 
-    // Custom Domain - DB Query
+  // Custom Domain - DB Query
 
-    const resolvesTo = `${BASE_PATH}${subdomain}`;
+  const resolvesTo = `${BASE_PATH}${subdomain}`;
 
-    console.log(resolvesTo);
-    return proxy.web(req, res, { target: resolvesTo, changeOrigin: true });
-  } catch (error) {
-    console.log("error", error);
-  }
+  console.log(resolvesTo);
+  return proxy.web(req, res, { target: resolvesTo, changeOrigin: true });
 });
 
 proxy.on("proxyReq", (proxyReq, req, res) => {
-  try {
-    const url = req.url;
-    console.log("req url", url);
-    console.log("call", proxyReq.path);
-    if (url === "/") {
-      console.log("if call");
-      proxyReq.path += "index.html";
-    } else {
-      const splitUrl = url.split("/");
-      const pathSplit = proxyReq.path.split("/");
-      for (let i = 0; i < splitUrl.length - 1; i++) {
-        pathSplit.pop();
-      }
-      proxyReq.path = pathSplit.join("/");
+  const url = req.url;
+  console.log("req url", url);
+  console.log("call", proxyReq.path);
+  if (url === "/") {
+    console.log("if call");
+    proxyReq.path += "index.html";
+  } else {
+    const splitUrl = url.split("/");
+    const pathSplit = proxyReq.path.split("/");
+    for (let i = 0; i < splitUrl.length - 1; i++) {
+      pathSplit.pop();
     }
-    console.log("final", proxyReq.path);
-  } catch (error) {
-    console.log("proxyReq error", error);
+    proxyReq.path = pathSplit.join("/");
   }
+  console.log("final", proxyReq.path);
 });
 
 app.listen(PORT, () => console.log(`Reverse Proxy Running..${PORT}`));
